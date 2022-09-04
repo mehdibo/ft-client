0b6b7f79dc803b7926cb0812c997e63a25095465
Mehdi Bounya <mehdi.bounya@gmail.com>
2022-09-04 15:11:19 +0100

    Create EnumerationRateLimited exception

---
+++ src/Exception/EnumerationRateLimited.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mehdibo\FortyTwo\Client\Exception;
+
+/**
+ * This exception is thrown when the rate limit is reached while enumerating
+ * It can be used to resume the enumeration later from the page where it stopped
+ */
+class EnumerationRateLimited extends \Exception
+{
+
+    public int|null $retryAfter;
+
+    /**
+     * @param int $reachedPage The page where the enumeration was stopped
+     */
+    public function __construct(public int $reachedPage, RateLimitReached $rateLimitReached)
+    {
+        $this->retryAfter = $rateLimitReached->retryAfter;
+        parent::__construct($rateLimitReached->getMessage(), $rateLimitReached->getCode(), $rateLimitReached);
+    }
+
+}
